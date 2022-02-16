5d056f5a95a92c049b0fbe3b9dcdf2b6ff929ec7
its-kayes <kayes.ek8@gmail.com>
2022-02-17 02:29:36 +0600

    Saving Part

---
+++ js/app.js
@@ -0,0 +1,42 @@
+document.getElementById('calculate').addEventListener('click', function(){
+    // Food Cost 
+    let food = document.getElementById('food');
+    let foodPriceText = food.value;
+    let foodPrice = parseInt(foodPriceText);
+
+    // Rent Cost 
+    let rent = document.getElementById('rent');
+    let rentPriceText = rent.value;
+    let rentPrice = parseInt(rentPriceText);
+
+    // Clothes Cost
+    let clothes = document.getElementById('clothes');
+    let clothesPriceText = clothes.value;
+    let clothesPrice = parseInt(clothesPriceText);
+
+    // Intotal Cost 
+    let totalCost = foodPrice + rentPrice + clothesPrice;
+    document.getElementById('total-exp').innerText = totalCost;
+    // let totalCostText = document.getElementById('total-exp');
+    // totalCostText.innerText = totalCost;
+
+    // Balance Part 
+    let income = document.getElementById('income');
+    let incomeTaka = parseInt(income.value);
+    document.getElementById('balance').innerText= incomeTaka - parseInt(totalCost);
+
+    console.log(totalCost);
+})
+// console.log('Hocche Maybe')
+
+document.getElementById('save-btn').addEventListener('click', function(){
+    let income = document.getElementById('income');
+    let incomeTaka = parseInt(income.value);
+
+    // Saving Part 
+    let save = document.getElementById('save');
+    let saveTaka = parseInt(save.value);
+    let savingTaka = incomeTaka * (saveTaka / 100 );
+    let savingTakaText = parseInt(savingTaka);
+    document.getElementById('SavedTaka').innerText = savingTakaText;
+})
