2f61b6cc66cb09b5c1b510a19f7007429fe76d25
its-kayes <kayes.ek8@gmail.com>
2022-02-17 14:00:08 +0600

    Error handling

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
-function monthlyIncome(){
+function monthlyIncome() {
     let income = document.getElementById('income');
     let incomeTaka = parseInt(income.value);
-    return incomeTaka;
+    if (incomeTaka < 0) {
+        document.getElementById('negative').style = 'block';
+        document.getElementById('income').value = '';
+    }
+    else {
+        return incomeTaka;
+    }
+
 }
 
 // function items(item){
@@ -11,7 +18,7 @@ function monthlyIncome(){
 // }
 
 
-document.getElementById('calculate').addEventListener('click', function(){
+document.getElementById('calculate').addEventListener('click', function () {
     // Food Cost 
     // items('food');
     let food = document.getElementById('food');
@@ -28,36 +35,59 @@ document.getElementById('calculate').addEventListener('click', function(){
     let clothesPriceText = clothes.value;
     let clothesPrice = parseInt(clothesPriceText);
 
-    // Intotal Cost 
-    let totalCost = foodPrice + rentPrice + clothesPrice;
-    document.getElementById('total-exp').innerText = totalCost;
-    // let totalCostText = document.getElementById('total-exp');
-    // totalCostText.innerText = totalCost;
+    if (foodPrice < 0 || rentPrice < 0 || clothesPrice < 0) {
+        document.getElementById('negative').style = 'block';
+        document.getElementById('food').value = '';
+        document.getElementById('rent').value = '';
+        document.getElementById('clothes').value = '';
+    }
+    else {
 
-    // Balance Part 
-    // let income = document.getElementById('income');
-    // let incomeTaka = parseInt(income.value);
-    let balance = monthlyIncome() - parseInt(totalCost);
-    document.getElementById('balance').innerText = balance;
+        // Intotal Cost 
+        let totalCost = foodPrice + rentPrice + clothesPrice;
+        document.getElementById('total-exp').innerText = totalCost;
+        // let totalCostText = document.getElementById('total-exp');
+        // totalCostText.innerText = totalCost;
 
-    console.log(totalCost);
+        // Balance Part 
+        // let income = document.getElementById('income');
+        // let incomeTaka = parseInt(income.value);
+        let balance = monthlyIncome() - parseInt(totalCost);
+        document.getElementById('balance').innerText = balance;
+
+        console.log(totalCost);
+    }
+
 })
 // console.log('Hocche Maybe')
 
-document.getElementById('save-btn').addEventListener('click', function(){
+document.getElementById('save-btn').addEventListener('click', function () {
     // let income = document.getElementById('income');
     // let incomeTaka = parseInt(income.value);
 
     // Saving Part 
     let save = document.getElementById('save');
     let saveTaka = parseInt(save.value);
-    let savingTaka = parseInt(monthlyIncome() * (saveTaka / 100 )); 
-    document.getElementById('SavedTaka').innerText = savingTaka;
+    if (saveTaka < 0) {
+        document.getElementById('negative').style = 'block';
+        document.getElementById('save').value = '';
+    }
+    else {
+        let savingTaka = parseInt(monthlyIncome() * (saveTaka / 100));
+        document.getElementById('SavedTaka').innerText = savingTaka;
 
-    // Remaning Balance 
-    let newBalanceText = document.getElementById('balance').innerText;
-    let newBalance = parseInt(newBalanceText);
-    let remaningBalance = newBalance - savingTaka;
-    console.log(remaningBalance);
-    document.getElementById('remaining-balance').innerText = remaningBalance;
+        // Remaning Balance 
+        let newBalanceText = document.getElementById('balance').innerText;
+        let newBalance = parseInt(newBalanceText);
+        let remaningBalance = newBalance - savingTaka;
+        if(remaningBalance < 0){
+            document.getElementById('error-balance').style = 'block';
+        }
+        else{
+            console.log(remaningBalance);
+            document.getElementById('remaining-balance').innerText = remaningBalance;
+        }
+        
+    }
+
 })
